cf9197aa1bef3f19ffe8be34bc2f93cc2bfb1dd5
kandoka <kandoka@126.com>
2020-05-05 21:17:11 +0800

    add clock, modify client

--- src/main/java/com/da/Client.java
+++ src/main/java/com/da/Client.java
@@ -5,24 +5,31 @@ import com.da.util.JsonUtil;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Client {
 
-    Long skew = 0l;
-
-    public Long getSkew() {
-        return skew;
-    }
-
-    public void setSkew(Long skew) {
-        this.skew = skew;
-    }
+    private Clock currentClock;
+    private int rate = 1000;
 
     public Client() throws IOException {
+        //create a clock for this machine and set its current time
+        currentClock = new Clock(new Date().getTime());
+        rate += new Random().nextInt(100);
+
+        //a thread to update clock with a specific frequency
+        new Thread(()->{
+            while(true){
+                try {
+                    Thread.sleep(1000);
+                    currentClock.updateCurrentTime(1l * rate);
+                    //System.out.println("+++++current time++++ "+currentClock.getCurrentTime());
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+
         Socket socket = new Socket("localhost",8090) ;
         BufferedReader br = null ;
 
@@ -30,24 +37,32 @@ public class Client {
         br = new BufferedReader(new InputStreamReader(socket.getInputStream())) ;
         String jsonStr = null ;
         while( (jsonStr = br.readLine()) != null)  {
-            Long currentTime = JsonUtil.json2long(jsonStr);
-            System.out.println("currentTime 是 ===>" + currentTime);
-            if(currentTime==-1l){   //确保是服务器发来的获取时间的请求，即值为-1
+            Long amountToAdjust = JsonUtil.json2long(jsonStr);
+            System.out.println("amount that follower must adjust its clock ===>" + amountToAdjust);
+            if(amountToAdjust == 0l){   //确保是服务器发来的获取时间的请求，即值为0
                 sendCurrentTime(socket);
             }
-            else{
-                //矫正当前漂移
-                setSkew(currentTime - new Date().getTime());
+            else{//clock needs to be adjusted
                 System.out.println("current machine time is: "+ new Date());
-                System.out.println("current server time is: "+ new Date(new Date().getTime()+getSkew()));
-                System.out.println("skew is: "+getSkew());
+                System.out.println("current server time is: "+ new Date(new Date().getTime() + amountToAdjust));
+                System.out.println("skew is: "+ amountToAdjust);
+                adjustClock(amountToAdjust);
             }
+        }
+    }
+
+    public void adjustClock(Long amountToAdjust) {
+        if(amountToAdjust > 0l) {//adjust clock forward
+            currentClock.updateCurrentTime(amountToAdjust);
+        }
+        else {//slow the clock TODO
+
         }
     }
 
     //收到服务器请求后，向服务器发送时间
     public void sendCurrentTime(Socket socket){
-        Long currentTime = getSkew() + new Date().getTime();
+        Long currentTime = new Date().getTime();
         String jsonStr = JsonUtil.long2Json(currentTime);
         PrintWriter out;
         try  {
---
+++ src/main/java/com/da/Clock.java
@@ -0,0 +1,29 @@
+package com.da;
+
+/**
+ * @author: Kandoka
+ * @createTime: 2020/05/05 19:22
+ * @description:
+ */
+
+public class Clock {
+    private Long currentTime;
+
+    Clock(Long currentTime) {
+        this.currentTime = currentTime;
+    }
+
+    /**
+     * see current time
+     */
+    public synchronized Long getCurrentTime() {
+        return currentTime;
+    }
+
+    /**
+     * update current time by an amount
+     */
+    public synchronized void updateCurrentTime(Long amountToAdjust) {
+        this.currentTime += amountToAdjust;
+    }
+}
